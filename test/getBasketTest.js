4e31acbd73ac93b94f64828a33b6e4d7ee6a2a3b
Phil Meehan <philip.meehan@gmail.com>
2015-11-22 21:13:53 +0000

    Bump to make-up 6

--- test/getBasketTest.js
+++ test/getBasketTest.js
@@ -1,103 +1,101 @@
-/* jslint node: true */
-/* jshint -W030 */
+/* eslint no-unused-expressions:0 */
 'use strict';
 
 var _ = require( 'lodash' );
 var chai = require( 'chai' );
 var chaiAsPromised = require( 'chai-as-promised' );
 chai.use( chaiAsPromised );
-var should;
-should = require( 'chai' ).should();
+chai.should();
 var sinon = require( 'sinon' );
 
 var getBasket = require( '../lib/getBasket' );
 
 // Stop the Tests from interfering with each other by returning clones of the fixtures
 function loadTestResource( location ) {
-	return _.cloneDeep( require( location ) );
+  return _.cloneDeep( require( location ) );
 }
 
 var successfulHarvestDb = {
-	get: function( id, callback ) {
-		callback( null, {} );
-	}
+  get: function( id, callback ) {
+    callback( null, {} );
+  }
 };
 
 var failedHarvestDb = {
-	get: function( id, callback ) {
-		callback( 'An error has occurred', null );
-	}
+  get: function( id, callback ) {
+    callback( 'An error has occurred', null );
+  }
 };
 
 var harvest = {
-	getSharedBasket: function() {
-		return {};
-	}
+  getSharedBasket: function() {
+    return {};
+  }
 };
 
 describe( 'LIB: getBasket', function() {
 
-	it( 'getBasket should throw an error if there is no options object', function() {
-		return getBasket( harvest, successfulHarvestDb ).then( function() {}, function( error ) {
-			error.should.have.property( 'error' ).that.is.an.instanceof( TypeError );
-			error.error.message.should.equal( 'invalid options' );
-			error.should.have.property( 'origin' ).that.is.equal( 'pluginHarvest' );
-		} );
-	} );
+  it( 'getBasket should throw an error if there is no options object', function() {
+    return getBasket( harvest, successfulHarvestDb ).then( function() {}, function( error ) {
+      error.should.have.property( 'error' ).that.is.an.instanceof( TypeError );
+      error.error.message.should.equal( 'invalid options' );
+      error.should.have.property( 'origin' ).that.is.equal( 'pluginHarvest' );
+    } );
+  } );
 
-	it( 'getBasket should throw an error if there is no options.id', function() {
-		return getBasket( harvest, successfulHarvestDb, loadTestResource( './fixtures/requestWithTag' ) ).then( function() {}, function( error ) {
-			error.should.have.property( 'error' ).that.is.an.instanceof( TypeError );
-			error.error.message.should.equal( 'invalid options.id' );
-			error.should.have.property( 'origin' ).that.is.equal( 'pluginHarvest' );
-		} );
-	} );
+  it( 'getBasket should throw an error if there is no options.id', function() {
+    return getBasket( harvest, successfulHarvestDb, loadTestResource( './fixtures/requestWithTag' ) ).then( function() {}, function( error ) {
+      error.should.have.property( 'error' ).that.is.an.instanceof( TypeError );
+      error.error.message.should.equal( 'invalid options.id' );
+      error.should.have.property( 'origin' ).that.is.equal( 'pluginHarvest' );
+    } );
+  } );
 
-	it( 'getBasket should throw an error if there is no options.tag', function() {
-		return getBasket( harvest, successfulHarvestDb, loadTestResource( './fixtures/requestWithId' ) ).then( function() {}, function( error ) {
-			error.should.have.property( 'error' ).that.is.an.instanceof( TypeError );
-			error.error.message.should.equal( 'invalid options.tag' );
-			error.should.have.property( 'origin' ).that.is.equal( 'pluginHarvest' );
-		} );
-	} );
+  it( 'getBasket should throw an error if there is no options.tag', function() {
+    return getBasket( harvest, successfulHarvestDb, loadTestResource( './fixtures/requestWithId' ) ).then( function() {}, function( error ) {
+      error.should.have.property( 'error' ).that.is.an.instanceof( TypeError );
+      error.error.message.should.equal( 'invalid options.tag' );
+      error.should.have.property( 'origin' ).that.is.equal( 'pluginHarvest' );
+    } );
+  } );
 
-	it( 'getBasket should reject if the document store throws an error', function() {
-		return getBasket( harvest, failedHarvestDb, loadTestResource( './fixtures/requestWithIdAndTag' ) ).then( function() {}, function( error ) {
-			error.error.should.equal( 'An error has occurred' );
-			error.should.have.property( 'origin' ).that.is.equal( 'pluginHarvest' );
-		} );
-	} );
+  it( 'getBasket should reject if the document store throws an error', function() {
+    return getBasket( harvest, failedHarvestDb, loadTestResource( './fixtures/requestWithIdAndTag' ) ).then( function() {}, function( error ) {
+      error.error.should.equal( 'An error has occurred' );
+      error.should.have.property( 'origin' ).that.is.equal( 'pluginHarvest' );
+    } );
+  } );
 
-	it( 'getBasket should get a basket from a document store based on the current id', function( done ) {
-		sinon.spy( successfulHarvestDb, 'get' );
-		getBasket( harvest, successfulHarvestDb, loadTestResource( './fixtures/requestWithIdAndTag' ) );
-		successfulHarvestDb.get.calledOnce.should.be.true;
-		successfulHarvestDb.get.restore();
-		done();
-	} );
+  it( 'getBasket should get a basket from a document store based on the current id', function( done ) {
+    sinon.spy( successfulHarvestDb, 'get' );
+    getBasket( harvest, successfulHarvestDb, loadTestResource( './fixtures/requestWithIdAndTag' ) );
+    successfulHarvestDb.get.calledOnce.should.be.true;
+    successfulHarvestDb.get.restore();
+    done();
+  } );
 
-	it( 'getBasket should ask Harvest to get a shared basket based on the basket stored in the document store', function( done ) {
-		sinon.spy( harvest, 'getSharedBasket' );
-		getBasket( harvest, successfulHarvestDb, loadTestResource( './fixtures/requestWithIdAndTag' ) );
-		harvest.getSharedBasket.calledOnce.should.be.true;
-		harvest.getSharedBasket.restore();
-		done();
-	} );
+  it( 'getBasket should ask Harvest to get a shared basket based on the basket stored in the document store', function( done ) {
+    sinon.spy( harvest, 'getSharedBasket' );
+    getBasket( harvest, successfulHarvestDb, loadTestResource( './fixtures/requestWithIdAndTag' ) );
+    harvest.getSharedBasket.calledOnce.should.be.true;
+    harvest.getSharedBasket.restore();
+    done();
+  } );
 
-	describe( 'exceptions thrown', function() {
+  describe( 'exceptions thrown', function() {
 
-		before( function() {
-			sinon.stub( harvest, 'getSharedBasket', function() {
-				throw new Error( 'Fake error' );
-			} );
-		} );
+    before( function() {
+      sinon.stub( harvest, 'getSharedBasket', function() {
+        throw new Error( 'Fake error' );
+      } );
+    } );
 
-		it( 'getBasket should reject if Harvest getSharedBasket throws an error', function() {
-			return getBasket( harvest, successfulHarvestDb, loadTestResource( './fixtures/requestWithIdAndTag' ) ).should.be.rejected.and.eventually.have.property( 'error' ).that.is.an.instanceof( Error );
-		} );
+    it( 'getBasket should reject if Harvest getSharedBasket throws an error', function() {
+      return getBasket( harvest, successfulHarvestDb, loadTestResource( './fixtures/requestWithIdAndTag' ) ).should.be.rejected.and.eventually.have.property( 'error' ).that.is.an.instanceof( Error );
+    } );
 
-		after( function() {
-			harvest.getSharedBasket.restore();
-		} );
-	} );
+    after( function() {
+      harvest.getSharedBasket.restore();
+    } );
+  } );
 } );
--- test/pluginHarvestTest.js
+++ test/pluginHarvestTest.js
@@ -1,13 +1,11 @@
-/* jslint node: true */
-/*jshint -W030 */
+/* eslint no-unused-expressions:0 */
 'use strict';
 
 var Hapi = require( 'hapi' );
 var chai = require( 'chai' );
 var chaiAsPromised = require( 'chai-as-promised' );
 chai.use( chaiAsPromised );
-var should;
-should = require( 'chai' ).should();
+chai.should();
 var sinon = require( 'sinon' );
 var rewire = require( 'rewire' );
 var Q = require( 'q' );
@@ -20,86 +18,86 @@ var server;
 var plugin;
 
 describe( 'LIB: pluginHarvest', function() {
-	var pluginFunction;
+  var pluginFunction;
 
-	before( function( done ) {
+  before( function( done ) {
 
-		// need to start up a server
-		server = new Hapi.Server();
-		server.methods.getService = function() {
-			return {
-				protocol: 'http',
-				hostname: 'localhost',
-				port: 5984,
-				authentication: {
-					username: 'test',
-					password: 'test'
-				}
-			};
-		};
-		// and then register this plugin to that server along with soe demo secureKeys
-		server.pack.register( {
-			plugin: pluginHarvest,
-			options: {}
-		}, function() {
-			// give easy access to our plugin
-			plugin = server.plugins['plugin-harvest'];
-			done();
-		} );
+    // need to start up a server
+    server = new Hapi.Server();
+    server.methods.getService = function() {
+      return {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: 5984,
+        authentication: {
+          username: 'test',
+          password: 'test'
+        }
+      };
+    };
+    // and then register this plugin to that server along with soe demo secureKeys
+    server.pack.register( {
+      plugin: pluginHarvest,
+      options: {}
+    }, function() {
+      // give easy access to our plugin
+      plugin = server.plugins['plugin-harvest'];
+      done();
+    } );
 
-		pluginFunction = function() {
-			return Q.resolve();
-		};
-	} );
+    pluginFunction = function() {
+      return Q.resolve();
+    };
+  } );
 
-	it( 'should allow us to access the plugin off the hapi server', function( done ) {
-		plugin.should.not.be.an( 'undefined' );
-		done();
-	} );
+  it( 'should allow us to access the plugin off the hapi server', function( done ) {
+    plugin.should.not.be.an( 'undefined' );
+    done();
+  } );
 
-	it( 'should expose getBasket as a function', function() {
-		pluginHarvest.__set__( 'getBasket', pluginFunction );
-		var getBasketSpy = sinon.spy( pluginFunction );
-		return plugin.getBasket().then( function() {
-			plugin.getBasket.should.be.a( 'function' );
-			getBasketSpy.should.be.calledOnce;
-		} );
-	} );
+  it( 'should expose getBasket as a function', function() {
+    pluginHarvest.__set__( 'getBasket', pluginFunction );
+    var getBasketSpy = sinon.spy( pluginFunction );
+    return plugin.getBasket().then( function() {
+      plugin.getBasket.should.be.a( 'function' );
+      getBasketSpy.should.be.calledOnce;
+    } );
+  } );
 
-	it( 'should expose getBaskets as a function', function() {
-		pluginHarvest.__set__( 'getBaskets', pluginFunction );
-		var getBasketsSpy = sinon.spy( pluginFunction );
-		return plugin.getBaskets().then( function() {
-			plugin.getBaskets.should.be.a( 'function' );
-			getBasketsSpy.should.be.calledOnce;
-		} );
-	} );
+  it( 'should expose getBaskets as a function', function() {
+    pluginHarvest.__set__( 'getBaskets', pluginFunction );
+    var getBasketsSpy = sinon.spy( pluginFunction );
+    return plugin.getBaskets().then( function() {
+      plugin.getBaskets.should.be.a( 'function' );
+      getBasketsSpy.should.be.calledOnce;
+    } );
+  } );
 
-	it( 'should expose postBasket as a function', function() {
-		pluginHarvest.__set__( 'postBasket', pluginFunction );
-		var postBasketSpy = sinon.spy( pluginFunction );
-		return plugin.postBasket().then( function() {
-			plugin.postBasket.should.be.a( 'function' );
-			postBasketSpy.should.be.calledOnce;
-		} );
-	} );
+  it( 'should expose postBasket as a function', function() {
+    pluginHarvest.__set__( 'postBasket', pluginFunction );
+    var postBasketSpy = sinon.spy( pluginFunction );
+    return plugin.postBasket().then( function() {
+      plugin.postBasket.should.be.a( 'function' );
+      postBasketSpy.should.be.calledOnce;
+    } );
+  } );
 
-	it( 'should expose putBasket as a function', function() {
-		pluginHarvest.__set__( 'putBasket', pluginFunction );
-		var putBasketSpy = sinon.spy( pluginFunction );
-		return plugin.putBasket().then( function() {
-			plugin.putBasket.should.be.a( 'function' );
-			putBasketSpy.should.be.calledOnce;
-		} );
-	} );
+  it( 'should expose putBasket as a function', function() {
+    pluginHarvest.__set__( 'putBasket', pluginFunction );
+    var putBasketSpy = sinon.spy( pluginFunction );
+    return plugin.putBasket().then( function() {
+      plugin.putBasket.should.be.a( 'function' );
+      putBasketSpy.should.be.calledOnce;
+    } );
+  } );
 
-	it( 'should expose postBasketVersion as a function', function() {
-		pluginHarvest.__set__( 'postBasketVersion', pluginFunction );
-		var postBasketVersionSpy = sinon.spy( pluginFunction );
-		return plugin.postBasketVersion().then( function() {
-			plugin.postBasketVersion.should.be.a( 'function' );
-			postBasketVersionSpy.should.be.calledOnce;
-		} );
-	} );
+  it( 'should expose postBasketVersion as a function', function() {
+    pluginHarvest.__set__( 'postBasketVersion', pluginFunction );
+    var postBasketVersionSpy = sinon.spy( pluginFunction );
+    return plugin.postBasketVersion().then( function() {
+      plugin.postBasketVersion.should.be.a( 'function' );
+      postBasketVersionSpy.should.be.calledOnce;
+    } );
+  } );
 
 } );
